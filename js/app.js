a4b4d489b6a5ae36e381ddc394929790f38f369f
Simone0730 <simone0730@hotmail.com>
2024-03-26 12:24:59 +0100

    esercizio

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let nome = prompt ("Inserisci il tuo nome") //string
+console.log(nome);
+
+let cognome = prompt ("Inserisci il tuo cognome") //string
+console.log (cognome);
+
+let colore = prompt ("Inserisci il tuo colore preferito") //string
+console.log (colore);
+
+const pass = nome+cognome+colore+21
+console.log (pass)
+
+
