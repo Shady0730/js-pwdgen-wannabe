aa109831e6dbce9eede8b341352f2c3857e17c99
Simone0730 <simone0730@hotmail.com>
2024-03-26 12:27:50 +0100

    sistemato errore

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ console.log (cognome);
 let colore = prompt ("Inserisci il tuo colore preferito") //string
 console.log (colore);
 
-const pass = nome+cognome+colore+21
+const numero= 21
+
+const pass = nome+cognome+colore+numero
 console.log (pass)
 
 
